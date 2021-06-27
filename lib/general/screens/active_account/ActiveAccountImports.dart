078b8450cce42f26fbb7b70757da7c6b8b5d15c8
tarekfouda <tarekfouda0@gmail.com>
2021-06-27 08:01:53 +0200

    GenericHttp

--- lib/general/screens/active_account/ActiveAccountImports.dart
+++ lib/general/screens/active_account/ActiveAccountImports.dart
@@ -1,6 +1,5 @@
 import 'package:base_flutter/general/constants/CustomButtonAnimation.dart';
 import 'package:base_flutter/general/resources/GeneralRepoImports.dart';
-import 'package:base_flutter/general/resources/GeneralRepoImports.dart';
 import 'package:base_flutter/general/widgets/AuthScaffold.dart';
 import 'widgets/ActiveAccountWidgetsImports.dart';
 import 'package:base_flutter/general/widgets/HeaderLogo.dart';
