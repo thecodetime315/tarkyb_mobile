f8541dde3219876a74c77cfe88ed91fc9109b9cb
tarekfouda <tarekfouda0@gmail.com>
2021-08-31 10:03:38 +0200

    call custom packages

--- lib/general/screens/login/widgets/LoginWidgetsImports.dart
+++ lib/general/screens/login/widgets/LoginWidgetsImports.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:base_flutter/general/screens/location_address/location_cubit/location_cubit.dart';
 import 'package:tf_custom_widgets/Inputs/GenericTextField.dart';
 import 'package:base_flutter/general/utilities/routers/RouterImports.gr.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +6,7 @@ import 'package:base_flutter/general/constants/MyColors.dart';
 import 'package:tf_custom_widgets/tf_custom_widgets.dart';
 import 'package:tf_validator/tf_validator.dart';
 import '../LoginImports.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 
 part 'BuildFormInputs.dart';
