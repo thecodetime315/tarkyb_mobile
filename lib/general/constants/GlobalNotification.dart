b824dfd62d7762c614b3b055302b2fa17cea0f1d
tarekfouda <tarekfouda0@gmail.com>
2021-10-28 10:06:45 +0200

    call custom packages

--- lib/general/constants/GlobalNotification.dart
+++ lib/general/constants/GlobalNotification.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:auto_route/auto_route.dart';
 import 'package:base_flutter/general/utilities/routers/RouterImports.gr.dart';
 import 'package:base_flutter/general/utilities/utils_functions/UtilsImports.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:dio/dio.dart';
 
 
 class GlobalNotification {
@@ -40,10 +43,10 @@ class GlobalNotification {
       // messaging.getInitialMessage().then((message) => _showLocalNotification(message));
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         print("_____________________Message data:${message.data}");
-        print("_____________________notification:${message.notification?.title}");
+        print("_____________________notification:${message.notification?.android?.imageUrl}");
         _showLocalNotification(message);
         _onMessageStreamController.add(message.data);
-        if (int.parse(message.data["type"]) == -1) {
+        if (int.parse(message.data["type"]??"0") == -1) {
           Utils.clearSavedData();
           AutoRouter.of(context).push(LoginRoute());
         }
@@ -68,19 +71,40 @@ class GlobalNotification {
 
   _showLocalNotification(RemoteMessage? message) async {
     if (message == null) return;
+    String? path;
+    BigPictureStyleInformation? bigPictureStyleInformation;
+    if (message.notification?.android?.imageUrl!=null) {
+      path = await _downloadAndSaveFile("${message.notification?.android?.imageUrl}");
+      bigPictureStyleInformation = BigPictureStyleInformation(
+        FilePathAndroidBitmap(path),
+        largeIcon: FilePathAndroidBitmap(path),
+        contentTitle: "${message.notification?.title}",
+        summaryText: "${message.notification?.body}",
+      );
+    }
+
     var android = AndroidNotificationDetails(
       "${DateTime.now()}",
       "DEFAULT",
       priority: Priority.high,
       importance: Importance.max,
       playSound: true,
+      largeIcon: path!=null? FilePathAndroidBitmap(path):null,
       shortcutId: DateTime.now().toIso8601String(),
+      styleInformation: bigPictureStyleInformation,
     );
     var ios = IOSNotificationDetails();
     var _platform = NotificationDetails(android: android, iOS: ios);
     _flutterLocalNotificationsPlugin.show(
         DateTime.now().microsecond, "${message.notification?.title}", "${message.notification?.body}", _platform,
         payload: json.encode(message.data));
+  }
+
+  Future<String> _downloadAndSaveFile(String url) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String filePath = '${directory.path}/${directory.path.split("/").last}';
+    await Dio().download(url,filePath);
+    return filePath;
   }
 
   static Future flutterNotificationClick(String? payload) async {
