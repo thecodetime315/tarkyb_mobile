14d55b7172202c4c2e504aa6d96c034d877bf1df
seif <seifabogheda89@gmail.com>
2023-05-28 02:12:40 +0300

    edit navigation service

--- lib/core/resource/navigation_service.dart
+++ lib/core/resource/navigation_service.dart
@@ -5,24 +5,24 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 class NavigationService {
   static BuildContext? currentContext = navigatorKey.currentContext;
 
+  static Route<dynamic> _materialPageRoute(Widget page) => MaterialPageRoute(builder: (_) => page);
 
-  static Future<dynamic>? navigateTo(String routeName, dynamic arguments) {
-    return navigatorKey.currentState
-        ?.pushNamed(routeName, arguments: arguments);
+  static Future<dynamic>? navigateTo(Widget page) {
+    return navigatorKey.currentState!.push(_materialPageRoute(page));
   }
 
   static void back() {
     return navigatorKey.currentState?.pop();
   }
 
-  static  Future<dynamic>? navigateAndReplacement(String routeName, dynamic arguments) {
-    return navigatorKey.currentState
-        ?.pushReplacementNamed(routeName, arguments: arguments);
+  static  Future<dynamic>? navigateAndReplacement(Widget page) {
+    return navigatorKey.currentState!.pushReplacement(_materialPageRoute(page));
   }
 
-  static Future<dynamic>? removeUntil(String routeName, dynamic arguments) {
-    return navigatorKey.currentState?.pushNamedAndRemoveUntil(
-        routeName, (Route<dynamic> route) => false,
-        arguments: arguments);
+  static Future<dynamic>? removeUntil(Widget page) {
+    return navigatorKey.currentState?.pushAndRemoveUntil(
+      _materialPageRoute(page),
+          (_) => false,
+    );
   }
 }
